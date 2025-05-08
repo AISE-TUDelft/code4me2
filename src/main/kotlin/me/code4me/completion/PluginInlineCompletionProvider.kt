e4e9f5d8b40e16efc3cb56639c30dae5e4cda675
Roham Koohestani <koohestaniroham@gmail.com>
2025-05-07 19:07:15 -0700

    [CORE] ghost-text completion: added the basic mechanism to show inline ghost-text completions to the user upon a request made.
    (cherry picked from commit 876ad786455994e608ff812e89daff98b71c5546)

---
+++ src/main/kotlin/me/code4me/completion/PluginInlineCompletionProvider.kt
@@ -0,0 +1,53 @@
+package me.code4me.completion
+
+import com.intellij.codeInsight.inline.completion.DebouncedInlineCompletionProvider
+import com.intellij.codeInsight.inline.completion.InlineCompletionEvent
+import com.intellij.codeInsight.inline.completion.InlineCompletionInsertHandler
+import com.intellij.codeInsight.inline.completion.InlineCompletionProviderID
+import com.intellij.codeInsight.inline.completion.InlineCompletionRequest
+import com.intellij.codeInsight.inline.completion.suggestion.InlineCompletionSuggestion
+import com.intellij.codeInsight.inline.completion.suggestion.InlineCompletionSuggestionUpdateManager
+import com.intellij.openapi.diagnostic.Logger
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
+
+class PluginInlineCompletionProvider: DebouncedInlineCompletionProvider() {
+    val LOGGER = Logger.getInstance("inlineCompletion")
+    private val PluginInsertHandler = PluginInlineCompletionInsertHandler()
+    private val PluginSuggestionUpdateManager = PluginInlineCompletionSuggestionUpdateManager(super.suggestionUpdateManager)
+
+
+    override suspend fun getSuggestionDebounced(request: InlineCompletionRequest): InlineCompletionSuggestion {
+        // TODO: implement this to actually collect the context send it to the server and get the response
+        // TODO: and then use that to create inline completion suggestion
+        LOGGER.info("Generating inline completion suggestion")
+        
+        // For testing, return a simple suggestion with some text
+        val document = request.editor.document
+        val requestId = request.requestId
+        val text = " // This is a sample completion"
+        
+        return PluginInlineCompletionSuggestion(
+            text,
+            requestId
+        )
+    }
+
+    override val insertHandler: InlineCompletionInsertHandler
+        get() = PluginInsertHandler
+
+    override val suggestionUpdateManager: InlineCompletionSuggestionUpdateManager
+        get() = PluginSuggestionUpdateManager
+
+    override suspend fun getDebounceDelay(request: InlineCompletionRequest): Duration {
+        return 250.toDuration(DurationUnit.MILLISECONDS)
+    }
+
+    override val id: InlineCompletionProviderID
+        get() = InlineCompletionProviderID("Code4Me V2")
+
+    override fun isEnabled(event: InlineCompletionEvent): Boolean {
+        return true
+    }
+}
---
+++ src/main/kotlin/me/code4me/completion/PluginInlineCompletionSuggestion.kt
@@ -0,0 +1,25 @@
+package me.code4me.completion
+
+import com.intellij.codeInsight.inline.completion.suggestion.InlineCompletionSuggestion
+import com.intellij.codeInsight.inline.completion.suggestion.InlineCompletionVariant
+
+class PluginInlineCompletionSuggestion(
+    private val completionText: String,
+    private val requestId: Long
+) : InlineCompletionSuggestion {
+
+    private var variants : List<InlineCompletionVariant>? = null
+
+    override suspend fun getVariants(): List<InlineCompletionVariant> {
+        if (variants == null) {
+            variants = listOf(
+                PluginInlineCompletionVariant(
+                    "some random testing text",
+                    requestId,
+                    "some random model"
+                )
+            )
+        }
+        return variants!!
+    }
+}
