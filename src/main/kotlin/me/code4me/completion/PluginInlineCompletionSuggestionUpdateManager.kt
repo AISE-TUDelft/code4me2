876ad786455994e608ff812e89daff98b71c5546
Roham Koohestani <koohestaniroham@gmail.com>
2025-05-07 19:07:15 -0700

    [CORE] ghost-text completion: added the basic mechanism to show inline ghost-text completions to the user upon a request made.

---
+++ src/main/kotlin/me/code4me/completion/PluginInlineCompletionSuggestionUpdateManager.kt
@@ -0,0 +1,19 @@
+package me.code4me.completion
+
+import com.intellij.codeInsight.inline.completion.InlineCompletionEvent
+import com.intellij.codeInsight.inline.completion.suggestion.InlineCompletionSuggestionUpdateManager
+import com.intellij.codeInsight.inline.completion.suggestion.InlineCompletionVariant
+
+class PluginInlineCompletionSuggestionUpdateManager(
+    private val baseManager: InlineCompletionSuggestionUpdateManager
+) : InlineCompletionSuggestionUpdateManager {
+    override fun update(
+        event: InlineCompletionEvent,
+        variant: InlineCompletionVariant.Snapshot
+    ): InlineCompletionSuggestionUpdateManager.UpdateResult {
+        // for now just call the base manager
+        // TODO: update this to actually handle the changes and return an updated version of the completions
+        return baseManager.update(event, variant)
+    }
+
+}
